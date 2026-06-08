7c9e190173fe7768f7099bd7884be90a24024176
Hector Zapatero <hzapatero32213@alumnos.uemc.es>
2026-06-08 17:45:06 +0200

    dewdwee

--- Trabajo Final MTPA/Cliente/VentanaChat.java
+++ Trabajo Final MTPA/Cliente/VentanaChat.java
@@ -191,7 +191,7 @@ public class VentanaChat extends JFrame {
             });
             
             areaChat.append(">> Conexión establecida con el servidor de la UEMC.\n");
-            areaChat.append(">> Comandos: REG|nombre | LOGIN|nombre|clave | MSG_ROOM|salon|mensaje o MSG_PRIV|usuario|mensaje\n");
+            areaChat.append(">> Comandos: REG|nombre | LOGIN|nombre|clave | MSG_ROOM|salon|mensaje o MSG_PRIV|usuario|mensaje O LIST_USERS\n");
             Servidor.GestorLogs.registrar("cliente.log", "Conexión exitosa con el servidor 127.0.0.1:5000");
         } catch (Exception e) {
             areaChat.append("Error al conectar: " + e.getMessage() + "\n");
